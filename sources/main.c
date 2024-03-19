a4ff7e04f119daa665ed87f3ab9ee8e51add8701
JsuisSayker <killian.trouve@epitech.eu>
2024-03-19 10:17:35 +0100

    [UPD] main.c
    complete the first step

--- sources/main.c
+++ sources/main.c
@@ -8,21 +8,27 @@ int main(void) {
   // do not remove please.
   setvbuf(stdout, NULL, _IONBF, 0);
 
-  /**
-   * EXAMPLE CODE WHICH WILL NOT WORK ...
-   * PLEASE PATCH IT ;-)
-   */
   char buffer[MAX_COMMAND_LENGTH + 1] = {0};
   int status = 0;
+  int buffer_index = 0;
+
   do {
-    memset(buffer, 0, MAX_COMMAND_LENGTH);
-    status = read(0, buffer, MAX_COMMAND_LENGTH);
+    status = read(0, buffer + buffer_index, 1); // read one character at a time
+
     if (status == -1) {
       perror("read()");
       return EPI_EXIT_FAILURE;
+    } else if (status == 0) {
+      return EPI_EXIT_SUCCESS;
     } else {
-      if (on_command(buffer) == false) {
-        return EPI_EXIT_FAILURE;
+      if (buffer[buffer_index] == '\n') { // if newline is encountered, process the command
+        if (on_command(buffer) == false) {
+          return EPI_EXIT_FAILURE;
+        }
+        memset(buffer, 0, MAX_COMMAND_LENGTH); // clear the buffer
+        buffer_index = 0; // reset the buffer index
+      } else {
+        buffer_index++; // increment the buffer index
       }
     }
   } while (status);
