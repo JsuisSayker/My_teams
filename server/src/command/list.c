5b561a84b71c0fafc4e488f2baa9fa768b246bca
arthurgauffre <arth.gauffre@gmail.com>
2024-04-21 23:03:47 +0200

    [UPD] Refactor list command to improve code readability and handle empty lists

--- server/src/command/list.c
+++ server/src/command/list.c
@@ -7,27 +7,47 @@
 
 #include "server.h"
 
+static int list_reply(char **message, message_t *reply,
+    client_server_t *client)
+{
+    append_to_string(message, client->context.thread->thread_uuid);
+    append_to_string(message, "|");
+    append_to_string(message, reply->sender_uuid);
+    append_to_string(message, "|");
+    append_to_string(message, reply->timestamp);
+    append_to_string(message, "|");
+    append_to_string(message, reply->message);
+    if (reply->entries.tqe_next != NULL)
+        append_to_string(message, "|");
+}
+
 static int list_all_reply(server_data_t *server, client_server_t *client)
 {
     char *message = NULL;
     message_t *reply = client->context.thread->messages.tqh_first;
 
-    append_to_string(&message, "200|/list|reply|");
-    TAILQ_FOREACH(reply, &client->context.thread->messages, entries) {
-        append_to_string(&message, client->context.thread->thread_uuid);
-        append_to_string(&message, "|");
-        append_to_string(&message, reply->sender_uuid);
-        append_to_string(&message, "|");
-        append_to_string(&message, reply->timestamp);
-        append_to_string(&message, "|");
-        append_to_string(&message, reply->message);
-        if (reply->entries.tqe_next != NULL)
-            append_to_string(&message, "|");
+    if (TAILQ_EMPTY(&client->context.thread->messages) == true) {
+        write(client->socket, "500|/list|no reply found\a\n", 22);
+        return OK;
     }
+    append_to_string(&message, "200|/list|reply|");
+    TAILQ_FOREACH(reply, &client->context.thread->messages, entries)
+        list_reply(&message, reply, client);
     append_to_string(&message, "\a\n");
     server_response(client->socket, message);
     free(message);
     return OK;
+}
+
+static int list_channel(char **message, channel_t *channel)
+{
+    append_to_string(message, channel->channel_uuid);
+    append_to_string(message, "|");
+    append_to_string(message, channel->channel_name);
+    append_to_string(message, "|");
+    append_to_string(message, channel->channel_description);
+    if (channel->entries.tqe_next != NULL)
+        append_to_string(message, "|");
 }
 
 static int list_all_channel(server_data_t *server, client_server_t *client)
@@ -35,16 +55,13 @@ static int list_all_channel(server_data_t *server, client_server_t *client)
     char *message = NULL;
     channel_t *channel = client->context.team->channels.tqh_first;
 
-    append_to_string(&message, "200|/list|channel|");
-    TAILQ_FOREACH(channel, &client->context.team->channels, entries) {
-        append_to_string(&message, channel->channel_uuid);
-        append_to_string(&message, "|");
-        append_to_string(&message, channel->channel_name);
-        append_to_string(&message, "|");
-        append_to_string(&message, channel->channel_description);
-        if (channel->entries.tqe_next != NULL)
-            append_to_string(&message, "|");
+    if (TAILQ_EMPTY(&client->context.team->channels) == true) {
+        write(client->socket, "500|/list|no channel found\a\n", 26);
+        return OK;
     }
+    append_to_string(&message, "200|/list|channel|");
+    TAILQ_FOREACH(channel, &client->context.team->channels, entries)
+        list_channel(&message, channel);
     append_to_string(&message, "\a\n");
     server_response(client->socket, message);
     free(message);
@@ -72,6 +89,10 @@ static int list_all_threads(server_data_t *server, client_server_t *client)
     char *message = NULL;
     thread_t *thread = client->context.channel->threads.tqh_first;
 
+    if (TAILQ_EMPTY(&client->context.channel->threads) == true) {
+        write(client->socket, "500|/list|no thread found\a\n", 25);
+        return OK;
+    }
     append_to_string(&message, "200|/list|thread|");
     TAILQ_FOREACH(thread, &client->context.channel->threads, entries) {
         if (list_thread(&message, thread) == ERROR)
@@ -83,21 +104,29 @@ static int list_all_threads(server_data_t *server, client_server_t *client)
     return OK;
 }
 
+static int list_team(char **message, team_t *team)
+{
+    append_to_string(message, team->team_uuid);
+    append_to_string(message, "|");
+    append_to_string(message, team->team_name);
+    append_to_string(message, "|");
+    append_to_string(message, team->team_description);
+    if (team->entries.tqe_next != NULL)
+        append_to_string(message, "|");
+}
+
 static int list_all_teams(server_data_t *server, client_server_t *client)
 {
     char *message = NULL;
     team_t *team = server->teams.tqh_first;
 
-    append_to_string(&message, "200|/list|team|");
-    TAILQ_FOREACH(team, &server->teams, entries) {
-        append_to_string(&message, team->team_uuid);
-        append_to_string(&message, "|");
-        append_to_string(&message, team->team_name);
-        append_to_string(&message, "|");
-        append_to_string(&message, team->team_description);
-        if (team->entries.tqe_next != NULL)
-            append_to_string(&message, "|");
+    if (TAILQ_EMPTY(&server->teams) == true) {
+        write(client->socket, "500|/list|no team found\a\n", 23);
+        return OK;
     }
+    append_to_string(&message, "200|/list|team|");
+    TAILQ_FOREACH(team, &server->teams, entries)
+        list_team(&message, team);
     append_to_string(&message, "\a\n");
     server_response(client->socket, message);
     free(message);
